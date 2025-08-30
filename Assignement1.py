7d3e48a6354eb0c66bfadc1bbbf44d5bd99f098e
lilypal <lilyannapalumbo@gmail.com>
2025-08-30 10:03:47 -0400

    Add files via upload

---
+++ Assignement1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 30 09:44:42 2025
+
+@author: lilya
+"""
+
+print("Grading and Submission Rules for CINF108")
+print("\n1. Submitted on time. Any assignment marked as late in Brightspace will have 0 points. Let’s be specific: Let's say the submission deadline on brightspace is 11:59PM, if your submission is timestamped as 11:59:01, Brightspace will mark it as late. Nothing I can do. If Brightspace says  your submission is late, it is late. No ifs, no buts, no discussion. PLease plan accordingly.")
+print("\n2. Quality. In this course, I will be running the code you write. If I can't successfully run your code, I will deduct points")
+print("\n3. Content. Please read the assignment carefully and make sure you comply with all the requirements listed in the assignment. Incomplete assignments wil have points deducted.")
+print("\n4. You MUST submit ensure that the code repository you are using is publicly accessible. I have provided instructions on how to create code repositories in GitHub for this class. If you want to use a different repository, you still must ensure it is accesssible and that your source files are downloadable.")
+print("\n5. In some instances, youu will be required to submit a screensht of the results of yourr program. Take pictures if needed and submit the .jpeg files.")
+print("\n6. All grades are individual")
